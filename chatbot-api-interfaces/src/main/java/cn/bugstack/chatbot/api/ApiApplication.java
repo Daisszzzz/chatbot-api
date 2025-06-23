11fa35cad7dfe820cb19472c79edf81df5739fe6
SD <dai.sszzzz@gmail.com>
2025-06-23 20:41:12 +0800

    项目初始化

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package cn.bugstack.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ApiApplication
+ * @Description 启动入口
+ * @Author lenovo
+ * @Date 2025/6/23 17:40
+ **/
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiApplication.class,args);
+
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/ApiTest.java
@@ -0,0 +1,11 @@
+package cn.bugstack.chatbot.api;
+
+/**
+ * @ClassName test
+ * @Author SD.
+ * @Description
+ * @Date 2025/6/23 20:21
+ **/
+public class ApiTest {
+}
+
